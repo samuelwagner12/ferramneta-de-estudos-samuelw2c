7819a5174d545676e7469aecd9169d14256631df
samuelwagner12 <162448824+samuelwagner12@users.noreply.github.com>
2024-11-21 08:43:26 -0300

    Add files via upload

---
+++ ferramenta de estudos samuel wagner/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é warzone',
+    'warzone é um jogo de tiro battle royale que o objetivo é ser o ultimo vivo para ganhar'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do méxico?',
+    'A capital do mexico é cidade do méxico'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz falar em Inglês?',
+    'falar em ingles é speak'
+)
